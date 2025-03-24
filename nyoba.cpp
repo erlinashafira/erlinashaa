403d9d78debf455fc7df2033bcbafd2e2ff5f736
errerew <erlina31acc@gmail.com>
2025-03-24 21:53:40 +0700

    rawr

--- nyoba.cpp
+++ nyoba.cpp
@@ -14,22 +14,10 @@ float luasLingkaran(float x){
         float panjang,lebar,jejari;
         do{
             cout << "===Menu===" << endl;
-            cout << "1. Menghtung Luas Persegi Panjang" << endl;
-            cout << "2. Menghitung Luaas Lingkaran" << endl;
-            cout << "3. Exit" << endl;
-            cout << "pilih (1/2/3) : ";
-            cin >> pilihan;
 
             switch(pilihan){
                 case 1 :
                     cout << "Menghitung Luas Persegi Panjang" << endl;
-                    cout << "Masukkan Panjang : ";
-                    cin >> panjang;
-                    cout << "Masukkan Lebar :";
-                    cin >> lebar;
-                    cout << "Luas Persegi Panjang"
-                    << luasPersegiPanjang(panjang,lebar) << endl;
-                    break;
             }
         }while(pilihan != 3);
     }
